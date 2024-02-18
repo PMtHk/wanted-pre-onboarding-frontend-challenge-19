ac9701ddf38b834507932821bce64d927d6bd8bc
PMtHk <zooby88@naver.com>
2024-02-18 18:13:55 +0900

    chore : update tailwind, postcss config

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      white: "#E5E7EB",
+      black: "#1E1E1E",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
